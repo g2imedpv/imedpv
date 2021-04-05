3c0b8cff59d331b78c65a808b0492c10a3376ed8
Yuli Li <yuli.li@g2-biopharma-services.com>
2021-04-05 14:33:29 -0400

    upload watermark pic

--- src/Controller/SdExportController.php
+++ src/Controller/SdExportController.php
@@ -1264,6 +1264,8 @@ class SdExportController extends AppController
                 $mpdf = new \Mpdf\Mpdf();
                 $mpdf->CSSselectMedia='mpdf';
                 $mpdf->SetTitle('FDA-MEDWATCH');
+                $mpdf->SetWatermarkImage('../img/draft-watermark.jpg');
+                $mpdf->showWatermarkImage = true;       
                 //$medwatchdata = $this->SdTabs->find();
                 $mpdf->use_kwt = true;
                 $mpdf->SetImportUse();
